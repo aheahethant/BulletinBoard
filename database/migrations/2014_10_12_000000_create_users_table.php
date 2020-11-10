00e7dcc562f2817906495837d905d49e75c7770c
AHEAHETHANT-PC\Ahe Ahe Thant <scm.aheahethant@gmail.com>
2020-11-10 11:02:18 +0630

    migration and model creation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,17 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile');
+            $table->string('type');
+            $table->string('phone');
+            $table->text('address');
+            $table->date('dob');
+            $table->integer('create_user_id');
+            $table->integer('updated_user_id');
+            $table->integer('deleted_user_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->datetime('deleted_at');
         });
     }
 
---
+++ database/migrations/2020_11_10_020920_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('status');
+            $table->unsignedBigInteger('create_user_id');
+            $table->integer('updated_user_id');
+            $table->integer('deleted_user_id');
+            $table->timestamps();
+            $table->datetime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
