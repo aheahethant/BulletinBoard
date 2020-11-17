1b1925c8f0507bf11f50e16840f5fd287f2547a3
AHEAHETHANT-PC\Ahe Ahe Thant <scm.aheahethant@gmail.com>
2020-11-17 16:27:25 +0630

    declared constant variables

--- public/js/user.js
+++ public/js/user.js
@@ -8,7 +8,8 @@ $.fn.dataTableExt.afnFiltering.push(
         }
         var dateStart = moment($("#from_date").val(), "YYYY-MM-DD").valueOf();
         var dateEnd = moment($("#to_date").val(), "YYYY-MM-DD").valueOf();
-        var evalDate = moment(aData[8], "YYYY/MM/DD").valueOf();
+        var created_date_column = 8;
+        var evalDate = moment(aData[created_date_column], "YYYY/MM/DD").valueOf();
         if (evalDate >= dateStart && evalDate <= dateEnd) {
             return true;
         } else {
@@ -39,7 +40,9 @@ $(document).ready(function () {
     $('#btnSearch').click(function () {
         var searchName = $('#name').val();
         var searchEmail = $('#email').val();
-        table.columns(1).search(searchName).draw();
-        table.columns(2).search(searchEmail).draw();
+        var name_column = 1;
+        var email_column = 2;
+        table.columns(name_column).search(searchName).draw();
+        table.columns(email_column).search(searchEmail).draw();
     });
 });
